1a20f28a3346e70a306ed5c49227f7a9ff1e7ea7
ASUS <ASUS@DESKTOP-D54TEVE>
2024-09-24 11:10:40 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_Tap_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bai 1
+            //Console.WriteLine("Nhap 2 so nguyen dương:");
+            //Console.WriteLine("Nhap a = ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Nhap b = ");
+            //int b = int.Parse(Console.ReadLine());
+            //int Tong = a + b;
+            //int Hieu = a - b;
+            //int Tich = a * b;
+            //float Thuong = (float)a / b;
+            //Console.WriteLine("Tong = {0}\nHieu = {1}\nTich = {2}\nThuong = {3}\n", Tong, Hieu, Tich, Thuong);
+            //Console.WriteLine();
+            //Console.ReadKey();
+
+            //Bai 2
+            //Console.WriteLine("Nhap chieu dai va chieu rong cua hinh chu nhat:");
+            //Console.WriteLine("Nhap chieu dai:");
+            //float chieu_dai = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Nhap chieu rong:");
+            //float chieu_rong = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Chu vi cua Hinh Chu Nhat = {0}", 2 * (chieu_dai + chieu_rong));
+            //Console.WriteLine("Dien tich cua Hinh Chu Nhat = {0}", (chieu_dai * chieu_rong));
+            //Console.ReadKey();
+
+            //Bai 3
+            //Console.WriteLine("Nhap duong kinh cua hinh tron:");
+            //const double pi = 3.14;
+            //double D = double.Parse(Console.ReadLine());
+            //double R = D / 2;
+            //double Chu_vi = D * pi;
+            //double Dien_tich = R * R * pi;
+            //Console.WriteLine("Ban kinh cua hinh tron = {0}", R);
+            //Console.WriteLine("Dien tich cua hinh tron = {0}", Dien_tich);
+            //Console.WriteLine("Chu vi cua hinh tron = {0}", Chu_vi);
+            //Console.ReadKey();
+
+            //Bai 4
+            //Console.WriteLine("Nhap thong tin hoc sinh:");
+            //Console.WriteLine("Nhap ho va ten hoc sinh:");
+            //string Ho_ten = Console.ReadLine();
+            //Console.WriteLine("Nhap lop:");
+            //string Lop = Console.ReadLine();
+            //Console.WriteLine("Nhap diem HKI:");
+            //float HKI = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Nhap diem HKII:");
+            //float HKII = float.Parse(Console.ReadLine());
+            //double DTB = (HKI + HKII * 2) / 3;
+            //Console.WriteLine("Diem trung binh = {0}", DTB);
+            //Console.ReadKey();
+
+            //Bai 5
+            //Console.WriteLine("nhap thong tin cho nhan vien 1:");
+            //Console.WriteLine("nhap ho ten: ");
+            //string hoten1 = Console.ReadLine();
+            //Console.WriteLine("nhap gioi tinh: ");
+            //string gioitinh1 = Console.ReadLine();
+            //Console.WriteLine("nhap ngay sinh: ");
+            //string ngaysinh1 = Console.ReadLine();
+
+            //Console.WriteLine("Nhap he so luong: ");
+            //double HeSoLuong1 = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Nhap luong co ban: ");
+            //double LuongCoBan1 = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Nhap so nam lam viec: ");
+            //int SoNamLamViec1 = int.Parse(Console.ReadLine());
+            //double PhuCap1 = SoNamLamViec1 > 5 ? LuongCoBan1 / 2 : 0;
+
+            //Console.WriteLine("\nNhap thong tin cho nhan vien 2:");
+            //Console.WriteLine("Nhap ho ten: ");
+            //string hoTen2 = Console.ReadLine();
+            //Console.WriteLine("Nhap gioi tinh: ");
+            //string gioiTinh2 = Console.ReadLine();
+            //Console.WriteLine("Nhap ngay sinh: ");
+            //string ngaySinh2 = Console.ReadLine();
+
+            //Console.WriteLine("Nhap he so luong: ");
+            //double HeSoLuong2 = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Nhap luong co ban: ");
+            //double LuongCoBan2 = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Nhap so nam lam viec: ");
+            //int SoNamLamViec2 = int.Parse(Console.ReadLine());
+            //double PhuCap2 = SoNamLamViec2 > 5 ? LuongCoBan2 / 2 : 0;
+
+            //Console.WriteLine("\nThong tin nhan vien 1:");
+            //Console.WriteLine("Ho ten: {0}", hoten1);
+            //Console.WriteLine("Gioi tinh: {0}", gioitinh1);
+            //Console.WriteLine("Ngay sinh: {0}", ngaysinh1);
+            //Console.WriteLine("He so luong: {0}", HeSoLuong1);
+            //Console.WriteLine("Luong co ban: {0}", LuongCoBan1);
+            //Console.WriteLine("So nam lam viec: {0}", SoNamLamViec1);
+            //Console.WriteLine("Phu cap nhan vien: {0}", PhuCap1);
+
+            //Console.WriteLine("\nThong tin nhan vien 2:");
+            //Console.WriteLine("Ho ten: {0}", hoTen2);
+            //Console.WriteLine("Gioi tinh: {0}", gioiTinh2);
+            //Console.WriteLine("Ngay sinh: {0}", ngaySinh2);
+            //Console.WriteLine("He so luong: {0}", HeSoLuong2);
+            //Console.WriteLine("Luong co ban: {0}", LuongCoBan2);
+            //Console.WriteLine("So nam lam viec: {0}", SoNamLamViec2);
+            //Console.WriteLine("Phu cap nhan vien: {0}", PhuCap2);
+            //Console.ReadKey();
+
+            //Bai 6
+            //Console.WriteLine("Nhap ngay thang nam:");
+            //Console.WriteLine("Nhap vao ngay ban muon:");
+            //int ngay = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Nhap vao thang ban muon:");
+            //int thang = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Nhap vao nam ban muon:");
+            //int nam = int.Parse(Console.ReadLine());
+            //DateTime inputDate = new DateTime(nam, thang, ngay);
+
+            //DateTime yesterday = inputDate.AddDays(-1);
+            //DateTime today = inputDate;
+            //DateTime tomorrow = inputDate.AddDays(1);
+
+            //Console.WriteLine("Ngày hôm nay là: {0}", today.ToString("dd/MM/yyyy"));
+            //Console.WriteLine("Ngày hien tai là: {0}", yesterday.ToString("dd/MM/yyyy"));
+            //Console.WriteLine("Ngày mai là: {0}", yesterday.ToString("dd/MM/yyyy"));
+
+            //Console.ReadKey();
+
+
+        }
+
+    }
+}
